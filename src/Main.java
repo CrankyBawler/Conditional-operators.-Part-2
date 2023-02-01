deaf05416d64a21a3e8e0efd1ae2c7554f180954
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-01 19:33:38 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,27 @@ public class Main {
         int clientOS = 1;
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                         }
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
+    }
+
     public static void task2() {
         System.out.println("Задача 2");
-
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
+
+}
