bb34529e7f94d968ec7f234d8bde193d53ca14ca
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-01 19:43:03 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
 
@@ -30,6 +31,16 @@ public class Main {
         }
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 1960;
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
     }
 
