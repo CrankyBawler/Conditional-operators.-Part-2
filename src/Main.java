3f688b43a0c5b520f20d785b1633523759bb7851
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-01 19:22:54 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        ;
+        task1();
+        task2();
+
     }
-}
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                         }
+        }
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
